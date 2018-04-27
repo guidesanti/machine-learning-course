9a9efbc88c3f05bf5928122b45ec793e9aa86def
Guilherme A. de Santi <g.santi@samsung.com>
2018-04-27 14:46:56 -0300

    Exercise 5
    Added implementation for exercise 5 part 2, bias-variance.

--- machine-learning-ex5/machine-learning-ex5/ex5/learningCurve.m
+++ machine-learning-ex5/machine-learning-ex5/ex5/learningCurve.m
@@ -51,15 +51,13 @@ error_val   = zeros(m, 1);
 %       end
 %
 
-% ---------------------- Sample Solution ----------------------
-
-
-
-
-
-
-
-% -------------------------------------------------------------
+for i = 1:m
+  [theta] = trainLinearReg(X(1:i,:), y(1:i), lambda);
+  [et, dummy] = linearRegCostFunction(X(1:i,:), y(1:i), theta, 0);
+  error_train(i) = et;
+  [ev, dummy] = linearRegCostFunction(Xval, yval, theta, 0);
+  error_val(i) = ev;
+end
 
 % =========================================================================
 
